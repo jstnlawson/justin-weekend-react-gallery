97af5e9114e35a33456d33cb30fc7c5f92fee236
Justin Lawson <jstn.lawson@gmail.com>
2023-07-07 16:08:23 -0500

    wrote git

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,14 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {   id: 1, 
+        path: 'images/goat_small.jpg', 
+        description: 'Photo of a goat taken at Glacier National Park.', 
+        likes: 0 
+    },
+    {   id: 2, 
+        path: 'images/goat_small.jpg', 
+        description: 'Photo of a GOAT taken at Glacier National Park.', 
+        likes: 1 
+    }
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,33 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
 import React from 'react';
+import GalleryList from './GalleryList/GalleryList'
 import './App.css';
 
 function App() {
+
+  const [picGallery, setPicGallery] = useState([])
+
+  const fetchGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/gallery'
+    })
+    .then((response) => {
+      console.log('Entire response:', response);
+        // The actual array comes from the data attribute on the response
+        console.log('Just the data:', response.data);
+        setPicGallery(response.data)
+    })
+    .catch(function (error) {
+      console.log('error on GET:', error)
+    })
+  }
+
+  useEffect( () => {
+    fetchGallery();
+  }, [])
+
     return (
       <div className="App">
         <header className="App-header">
